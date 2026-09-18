138715e9aadf979cff56dc7f90ba7bad9dfacac8
Martín Alejandro Argüello <43881938@fi.unju.edu.ar>
2026-09-18 01:24:46 -0300

    Agrega metodo para comparar cartas.

--- src/ar/edu/unju/fi/ed2026/utils/CartaUils.java
+++ src/ar/edu/unju/fi/ed2026/utils/CartaUils.java
@@ -81,4 +81,32 @@ public class CartaUils {
             System.out.println(carta.toString());
         }
     }
+
+    
+    
+    /**
+     * Compara las cartas en juego en un turno.
+     * @param cartas El arreglo de cartas que representa las cartas en juego.
+     * @return la carta mayor, o null si hay empate.
+     */
+    public Carta comparaCartas(Carta[] cartas) {
+        Carta ganadora = cartas[0];
+        boolean hayEmpate = false;
+
+        for (int i = 1; i < cartas.length; i++) {
+            Carta actual = cartas[i];
+
+            if (actual.getValor() > ganadora.getValor()) {
+                ganadora = actual;
+                hayEmpate = false; 
+                
+            } else if (actual.getValor() == ganadora.getValor()) {
+                hayEmpate = true; 
+            }
+        }
+        if (hayEmpate) {
+            return null;
+        }
+        return ganadora;
+    }
 }
